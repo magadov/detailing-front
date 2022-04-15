197bae760511571d782cd0f3a76e341ba3e4b6b3
Vis <awwya@mail.ru>
2022-04-15 18:13:52 +0300

    Вывел макет таблицы журнала услуг

---
+++ src/components/Pages/JournalPage/JournalPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Journal from '../../Journale/Journal';
+import Header from '../../Header/Header';
+
+const JournalPage = () => {
+  return (
+    <div>
+      <Header />
+      <Journal />
+    </div>
+  );
+};
+
+export default JournalPage;
