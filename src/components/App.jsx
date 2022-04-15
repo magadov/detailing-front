406fdaa05e572a5b1eeaa01324a16a69c232ffb3
Vis <awwya@mail.ru>
2022-04-15 15:52:11 +0300

    Добавлена страница авторизации и хидер

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// import SignInPage from "./Pages/SignIn/SignInPage";
+import { Route ,Routes } from "react-router-dom";
+import Header from './Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      {/*<Route path="/signIn" element={<SignInPage />} />*/}
+    <Route path='/' element={<Header />} />
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import { Provider } from "react-redux";
+import { store } from "./redux/configureStore";
+import { BrowserRouter } from "react-router-dom";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-root.render(<App />)
-
-
-
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
