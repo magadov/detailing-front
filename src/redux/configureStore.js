4c72b0295db7ca3d1ad70233312b8b804c40530d
usman <dzakaev.94@mail.ru>
2022-04-29 14:56:01 +0300

    добавление клиента

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -4,12 +4,16 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import application from "./features/application";
 import { materialReducer } from "./features/materialReducer";
 import { servicesReducer } from './features/services.reducer';
+import { clientsReducer } from './features/clients.reducer';
+import { carsReducer } from './features/cars.reducer';
 
 export const store = createStore(
   combineReducers({
     application,
     materialReducer,
-    servicesReducer
+    servicesReducer,
+    clientsReducer,
+    carsReducer
   }),
   composeWithDevTools(applyMiddleware(thunk))
 );
