f2e1e59ea63fd25e45bfecd4370c7ca796464701
Ali <aliumarov90@mail.ru>
2022-05-09 15:09:46 +0300

    Немного доработал код

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { DataGrid } from "@mui/x-data-grid";
 import {
   Button,
   Paper,
@@ -9,23 +8,27 @@ import {
   TableContainer,
   TableHead,
   TableRow,
-  TextField
+  TextField,
 } from "@mui/material";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import css from "./expenses.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import { loadMaterial } from "../../redux/features/materialReducer";
-import Admission from "./Admission";
 
+import NewMaterial from "./NewMaterial";
+import AdmissionModal from "./Admission";
+import {
+  deleteMaterial,
+  loadMaterial,
+} from "../../redux/actions/materialActions";
+import Edit from "./Edit";
 
 export default function Expenses() {
-  const materials = useSelector(
-    (state) => state.materialReducer.materials.material
-  );
+  const materials = useSelector((state) => state.materialReducer.materials);
+  const loading = useSelector((state) => state.materialReducer.loading);
+  const deleting = useSelector((state) => state.materialReducer.deleting);
   const [search, setSearch] = React.useState("");
-  const [open, setOpen] = React.useState(false);
 
   const dispatch = useDispatch();
 
@@ -33,44 +36,33 @@ export default function Expenses() {
     dispatch(loadMaterial());
   }, [dispatch]);
 
-  const handleOpenModal = () => {
-    setOpen(true);
+  const handleDeleteMaterial = (id) => {
+    dispatch(deleteMaterial(id));
   };
 
   const searchInputStyle = { width: 400 };
   const dateFormStyle = { width: 200 };
 
-  if (!materials) {
-    return "load";
-  }
-
-  const filtered = materials?.filter((element) => {
-    return element.name?.toLowerCase().includes(search.toLowerCase());
-  });
-
-  const rows = filtered.map((item, index) => {
-    return {
-      id: index + 1,
-      lastName: item.left,
-      firstName: item.name,
-      age: item.price,
-      fullName: item.direction.date,
-    };
+  const filtered = materials.filter((element) => {
+    return element.name.toLowerCase().includes(search.toLowerCase());
   });
 
   return (
     <>
-      <Container maxWidth="lg">
-        <Box sx={{ height: "100vh" }}>
-          <Box className={css.inputFormMain}>
-            <TextField
+      {loading ? (
+        "Загрузка"
+      ) : (
+        <Container maxWidth="lg">
+          <Box sx={{ height: "100vh" }}>
+            <Box className={css.inputFormMain}>
+              <TextField
                 id="outlined-basic"
                 label="Поиск"
                 variant="outlined"
                 style={searchInputStyle}
                 onChange={(event) => setSearch(event.target.value)}
-            />
-            <TextField
+              />
+              <TextField
                 id="date"
                 label="Дата"
                 type="date"
@@ -79,70 +71,86 @@ export default function Expenses() {
                 InputLabelProps={{
                   shrink: true,
                 }}
-            />
-            <Admission />
-          </Box>
-          <Box className={css.content}>
-            <h1> Список расходов </h1>
-            <TableContainer style={{width: 1050}} component={Paper}>
-              <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell align="right" >№</TableCell>
-                    <TableCell align="right">Наименование</TableCell>
-                    <TableCell align="right" >Цена</TableCell>
-                    <TableCell align="right">Количество</TableCell>
-                    <TableCell align="right">Дата</TableCell>
-                    <TableCell align="right">  </TableCell>
-                    <TableCell align="right">   </TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {filtered.map((row, index) => (
+              />
+              <NewMaterial />
+            </Box>
+            <Box className={css.content}>
+              <h1> Список расходов </h1>
+              <TableContainer style={{ width: 1050 }} component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell align="center">№</TableCell>
+                      <TableCell align="center" style={{ width: 250 }}>
+                        Наименование
+                      </TableCell>
+                      <TableCell align="center" style={{ width: 10 }}>
+                        Цена
+                      </TableCell>
+                      <TableCell align="center">Количество</TableCell>
+                      <TableCell style={{ width: 50 }}> </TableCell>
+                      <TableCell align="center">Единица</TableCell>
+                      <TableCell align="center" style={{ width: 100 }}>
+                        Дата
+                      </TableCell>
+                      <TableCell align="center" style={{ width: 10 }}>
+                        {" "}
+                      </TableCell>
+                      <TableCell align="center" style={{ width: 10 }}>
+                        {" "}
+                      </TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {filtered.map((row, index) => (
                       <TableRow
-                          key={index}
-                          sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                        key={index}
+                        sx={{
+                          "&:last-child td, &:last-child th": { border: 0 },
+                        }}
                       >
-                        <TableCell align="right" style={{width: 70}}>{index + 1}</TableCell>
+                        <TableCell align="right" style={{ width: 70 }}>
+                          {index + 1}
+                        </TableCell>
                         <TableCell component="th" scope="row">
                           {row.name}
                         </TableCell>
-                        <TableCell align="right">{row.price}</TableCell>
-                        <TableCell align="right">{row.left}</TableCell>
-                        <TableCell align="right">{row.direction.data}</TableCell>
+                        <TableCell align="center">{row.price}</TableCell>
+                        <TableCell align="center" style={{ width: 50 }}>
+                          {row.left}
+                        </TableCell>
+                        <TableCell align="center">
+                          <AdmissionModal materialId={row._id} />
+                        </TableCell>
+                        <TableCell align="center" style={{ width: 50 }}>
+                          {row.volumeType}
+                        </TableCell>
                         <TableCell align="right">
-                          <Button
-                              variant="contained"
-                              color="primary"
-                              style={{ background: "orange", fontSize: 8 }}
-                              // onClick={(event) => {
-                              //   handleClick(event, cellValues);
-                              // }}
-                          >
-                            изменить
-                          </Button>
+                          {row.direction.date}
+                        </TableCell>
+                        <TableCell align="right">
+                          <Edit materialId={row._id} />
                         </TableCell>
                         <TableCell align="right">
                           <Button
-                              variant="contained"
-                              color="primary"
-                              style={{ background: "orange", fontSize: 8 }}
-                              // onClick={(event) => {
-                              //   handleClick(event, cellValues);
-                              // }}
+                            variant="contained"
+                            color="primary"
+                            disabled={deleting}
+                            style={{ background: "orange", fontSize: 8 }}
+                            onClick={() => handleDeleteMaterial(row._id)}
                           >
                             удалить
                           </Button>
                         </TableCell>
-
                       </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Box>
           </Box>
-        </Box>
-      </Container>
+        </Container>
+      )}
     </>
   );
 }
